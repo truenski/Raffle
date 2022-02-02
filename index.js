4a7351478c57cbc76e47df24e756e1960d92f037
Kesney Mendes <kesneymendes@gmail.com>
2022-02-02 09:53:42 -0300

    Raffle Project

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+        let pessoas = ['igooor','jose','marcos','pamela','joana']
+        
+        
+        function sortear(){
+        let np = pessoas.length;
+        let ns = Math.floor(Math.random() * np);
+        document.getElementById("d").innerHTML = pessoas[ns];
+        console.log(ns)
+        
+        }
+        let pessoas = ['igooor','jose','marcos','pamela','joana']
+        
+        
+        function sortear(){
+        let np = pessoas.length;
+        let ns = Math.floor(Math.random() * np);
+        document.getElementById("d").innerHTML = pessoas[ns];
+        console.log(ns)
+        
+        }
